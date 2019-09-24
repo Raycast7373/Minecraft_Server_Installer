13a831fea554ae1a7d55821a3cf54dd00e00a1d0
QuintenBrala <q.brala@gmail.com>
2019-09-24 19:40:18 +0200

    TRY6

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ read -r -p "This Script Will Install Programms On Your Nas, Do You Wish To Conti
 case $input in
     [yY][eE][sS]|[yY])
  echo "Yes"
- sudo apt update && sudo apt install default-jre default-jdk screen -y && sudo mkdir /Minecraft && sudo cp ./eula.txt /Minecraft && sudo cp ./server.properties /Minecraft && cp ./StartScript.sh /Minecraft && sudo chmod +x /Minecraft/StartScript.sh && sudo bash ./ChooseMinecraftVersion.sh
+ sudo apt update && sudo apt install wget default-jre default-jdk screen -y && sudo mkdir /Minecraft && sudo cp ./eula.txt /Minecraft && sudo cp ./server.properties /Minecraft && cp ./StartScript.sh /Minecraft && sudo chmod +x /Minecraft/StartScript.sh && sudo bash ./ChooseMinecraftVersion.sh
  ;;
     [nN][oO]|[nN])
  echo "No"
