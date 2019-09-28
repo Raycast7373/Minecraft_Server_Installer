ffb457e1fda1f33e3da600913eb15250bbec03ce
QuintenBrala <q.brala@gmail.com>
2019-09-28 21:23:00 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ read -r -p "This Script Will Install Programms On Your Nas, Do You Wish To Conti
 case $input in
     [yY][eE][sS]|[yY])
  echo "Yes"
- sudo apt update && sudo apt upgrade -y && sudo apt install nano cron wget default-jre default-jdk screen -y && sudo mkdir /Minecraft && sudo cp ./eula.txt /Minecraft && sudo cp ./startmcs.sh / && sudo cp ./server.properties /Minecraft && sudo cp ./StartScript.sh /Minecraft && sudo chmod +x /Minecraft/StartScript.sh && sudo cp ./StartScript_2GB.sh /Minecraft && sudo chmod +x /Minecraft/StartScript_2GB.sh && sudo bash ./ChooseMinecraftVersion.sh
+ sudo apt update && sudo apt upgrade -y && sudo apt install nano cron wget default-jre default-jdk screen -y && sudo mkdir /Minecraft && sudo cp ./eula.txt /Minecraft && sudo cp ./startmcs.sh / && sudo cp ./server.properties /Minecraft && sudo cp ./StartScript.sh /Minecraft && sudo chmod +x /Minecraft/StartScript.sh &&sudo cp ./server.sh /Minecraft && sudo chmod +x /Minecraft/server.sh && sudo bash ./ChooseMinecraftVersion.sh
  ;;
     [nN][oO]|[nN])
  echo "No"
