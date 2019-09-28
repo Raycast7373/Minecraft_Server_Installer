e3bdf207ce1c591d4761e0441806804f001f2510
QuintenBrala <q.brala@gmail.com>
2019-09-28 17:53:54 +0200

    Update Download_Forge.sh

--- Download_Forge.sh
+++ Download_Forge.sh
@@ -1,2 +1,2 @@
-sudo wget -O server.jar -P ./MinecraftServerJars https://repo.spongepowered.org/maven/org/spongepowered/spongeforge/1.12.2-2825-7.1.6/spongeforge-1.12.2-2825-7.1.6.jar
+sudo wget ‐‐output-document=server.jar ‐‐directory-prefix=./MinecraftServerJars https://repo.spongepowered.org/maven/org/spongepowered/spongeforge/1.12.2-2825-7.1.6/spongeforge-1.12.2-2825-7.1.6.jar
 sudo bash nxt.sh
