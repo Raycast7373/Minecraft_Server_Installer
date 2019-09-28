119f113641711bd1f421636933bbed4d8f20a079
QuintenBrala <q.brala@gmail.com>
2019-09-28 14:27:55 +0200

    Bug Fix

--- ChooseMinecraftVersion.sh
+++ ChooseMinecraftVersion.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
  
-read -r -p "This Script Will Let You Choose Your Minecraft Server Version, To Choose CraftBukkit (1.14.2), Spigot (1.14.2), Vanila (1.14.2) Or SpongeForge (1.12.2). To Choose Type The Name Of The Type Minecraft, Example : bukkit  " input
+read -r -p "This Script Will Let You Choose Your Minecraft Server Version, To Choose CraftBukkit (1.14.2), Spigot (1.14.2), Vanila (1.14.2) Or SpongeForge (1.12.2). To Choose Type The First Letter Of The Server You Want Or If You Want To Exit Press e / E, For SpongeForge Type f / F. Example : bukkit = b / B " input
  
 case $input in
-    [b][bukkit][Bukkit][CraftBukkit][craftbukkit]|[b])
+    [b][B]|[b])
  echo "CraftBukkit Selected"
 sudo bash Download_Bukkit.sh
  ;;
-    [spigot][Spigot]|[spigot])
+    [s][S]|[s])
  echo "Spigot Selected"
 sudo bash Download_Spigot.sh
        ;;
-    [vanila][Vanila]|[vanila])
+    [v][V]|[v])
  echo "Vanila Selected"
 sudo bash Download_Vanila.sh
        ;;
-    [SpongeForge][spongeforge][sponge][Sponge][Forge][forge]|[SpongeForge])
+    [f][F]|[f])
  echo "SpongeForge Selected"
 sudo bash Download_Forge.sh
        ;;
-    [exit]|[exit])
+    [e][E]|[e])
  echo "Closing Script..."
+ sudo bash nxt.sh
  exit 1
  ;;
 esac
