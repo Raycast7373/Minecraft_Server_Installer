b5378415f55e64b9d2f0a0a9ef8fe49402c2d131
QuintenBrala <q.brala@gmail.com>
2019-09-28 17:54:15 +0200

    Update Download_Spigot.sh

--- Download_Spigot.sh
+++ Download_Spigot.sh
@@ -1,2 +1,2 @@
-sudo wget -O server.jar -P ./MinecraftServerJars https://cdn.getbukkit.org/spigot/spigot-1.14.4.jar 
+sudo wget ‐‐output-document=server.jar ‐‐directory-prefix=./MinecraftServerJars https://cdn.getbukkit.org/spigot/spigot-1.14.4.jar 
 sudo bash nxt.sh
