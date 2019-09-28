345a351fe92d1326902800e20e2d80d66bd51927
QuintenBrala <q.brala@gmail.com>
2019-09-28 18:04:06 +0200

    Update Download_Bukkit.sh

--- Download_Bukkit.sh
+++ Download_Bukkit.sh
@@ -1,2 +1,5 @@
-sudo wget ‐‐output-document=server.jar ‐‐directory-prefix=./MinecraftServerJars https://cdn.getbukkit.org/craftbukkit/craftbukkit-1.14.4-R0.1-SNAPSHOT.jar 
+cd ./MinecraftServerJars
+sudo wget https://cdn.getbukkit.org/craftbukkit/craftbukkit-1.14.4-R0.1-SNAPSHOT.jar 
+cp * ./server.jar
+cd ../
 sudo bash nxt.sh
