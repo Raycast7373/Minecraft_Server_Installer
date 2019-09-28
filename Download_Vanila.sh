91e8d8f17f13efeb512eedf6682ce64430c1ff63
QuintenBrala <q.brala@gmail.com>
2019-09-28 18:06:10 +0200

    Update Download_Vanila.sh

--- Download_Vanila.sh
+++ Download_Vanila.sh
@@ -1,2 +1,5 @@
-sudo wget ‐‐output-document=server.jar ‐‐directory-prefix=./MinecraftServerJars https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar
+cd ./MinecraftServerJars
+sudo wget https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar
+cp * ./server.jar
+cd ../
 sudo bash nxt.sh
